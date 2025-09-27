a43d1e30b21dde5441b4a99f083fe0bb308840e5
Anand Gupta <anandgupta@Anands-MacBook-Air.local>
2025-09-27 04:09:12 -0700

    Reusable Drawer Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import React, { useState } from 'react'
+import Drawer from './Components/Shared/Drawer'
 import Button from './Components/Shared/Button'
 import Card from './Components/Shared/Card'
 function App() {
+  const [open, setOpen] = useState(false)
   return (
-    <div className='p-16 grid grid-cols-2 gap-10'>
-    <Card 
-    title='Dashboard'
-    footer = {
-      <Button type='warning'>Edit</Button>
-    }
-    >
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, cum. Nam, animi libero modi natus error nobis quasi accusamus vitae iste sit harum laboriosam eius iure voluptates, quis recusandae nisi.
-    </Card>
-    <Card 
-    title='Profile'
-    footer = 'Hello I am Footer'
-    >Here is my Content</Card>
+    <div className='p-16'>
+      <Button type='secondary' onClick={()=>setOpen(true)}></Button>
+      <Drawer
+      open={open}
+      close={()=>setOpen(false)}
+      title='Sign In'
+      >
+        <Card title='Register Now'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At accusantium commodi maxime qui ad quidem incidunt et illo! Commodi exercitationem quod quia repudiandae possimus laudantium velit voluptatibus architecto consectetur ab.
+        </Card>
+      </Drawer>
     </div>
   )
 }
