4d178fe5cefeace47edb9714a292bd43925d98e6
Anand Gupta <anandgupta@Anands-MacBook-Air.local>
2025-09-27 19:53:32 -0700

    Form Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import React, { useState } from 'react'
-import Drawer from './Components/Shared/Drawer'
+import Form from "./Components/Shared/Form"
+import Input from "./Components/Shared/Input"
 import Button from './Components/Shared/Button'
 import Card from './Components/Shared/Card'
-function App() {
-  const [open, setOpen] = useState(false)
-  return (
-    <div className='p-16'>
-     <h1>You can design your own Model</h1>
+
+const App = ()=>{
+  return(
+  <div className="gr-gray-300 flex items-center justify-center h-screen">
+    <div className="w-4/12">
+    <Card title="Register Now">
+      <Form>
+        <div className="space-y-5">
+          <Input 
+          name='first-name'
+          type="text"
+          placeholder='Firstname'
+          />
+
+          <Input 
+          name='last-name'
+          type="text"
+          placeholder='Lastname'
+          />
+
+          <Input 
+          name='email'
+          type="email"
+          placeholder='email'
+          />
+
+          <Button type="secondary" icon='login-circle-line'>Login</Button>
+        </div>
+      </Form>
+    </Card>
     </div>
+  </div>
   )
 }
-
 export default App
--- src/Components/Shared/Card.jsx
+++ src/Components/Shared/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({title ='Card Title', children='Your Content', footer})=>{
     return(
-        <div className="shadow-lg p-8 rounded-lg border border-gray-100 space-y-2">
+        <div className="bg-white shadow-lg p-8 rounded-lg border border-gray-100 space-y-2">
             <h1 className="text-xl font-bold capitalize">{title}</h1>
 
             <div className="text-gray-500 font-semibold mt-2">
