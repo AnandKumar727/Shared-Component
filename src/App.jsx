6be16963925a019c0a9c0a9770e998038c891d4f
Anand Gupta <anandgupta@Anands-MacBook-Air.local>
2025-09-27 18:36:06 -0700

    Design Model Component For Model

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,7 @@ function App() {
   const [open, setOpen] = useState(false)
   return (
     <div className='p-16'>
-      <Button type='secondary' onClick={()=>setOpen(true)}></Button>
-      <Drawer
-      open={open}
-      close={()=>setOpen(false)}
-      title='Sign In'
-      >
-        <Card title='Register Now'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At accusantium commodi maxime qui ad quidem incidunt et illo! Commodi exercitationem quod quia repudiandae possimus laudantium velit voluptatibus architecto consectetur ab.
-        </Card>
-      </Drawer>
+     <h1>You can design your own Model</h1>
     </div>
   )
 }
